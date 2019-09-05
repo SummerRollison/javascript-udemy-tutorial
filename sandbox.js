c6038f8b4037ee14ab0d3fdaeaf74c72cacbae81
SummerRollison <me@summerrollison.com>
2019-09-05 17:22:16 -0500

    finished lesson 17

--- sandbox.js
+++ sandbox.js
@@ -1,15 +1,31 @@
-let age = 25;
+// type conversion
 
-// loose comparison (differnt types can still be equal)
+let score = '100';
+console.log(score + 1);
+// output: 1001 
+// need type conversion to turn string into number
 
-console.log(age == 25); // true
-console.log(age == '25'); // true
-console.log(age != 25); // false
-console.log(age != '25'); // false
+score = Number(score); // turnes score into a number
+console.log(score + 1);
+// output: 101
 
-// strict comparison (different types cannot be equal)
+console.log(typeof score);
+// output: number
 
-console.log(age === 25); // true
-console.log(age === '25'); // false
-console.log(age !== 25); // false
-console.log(age !== '25'); // true
+let result = Number('hello');
+console.log(result);
+// output: NaN
+
+let result1 = String(50);
+console.log(typeof result1);
+
+let result2 = Boolean(100);
+console.log(result2);
+// output: true
+
+let result3 = Boolean(0);
+console.log(result3);
+// output: false
+
+let result4 = Boolean('0');
+console.log(result4); //true, strings of any length are true. no length equals false

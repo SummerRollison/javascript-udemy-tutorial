25879c115479a41dd3d39edc0f53bd81b8ae6cdc
SummerRollison <me@summerrollison.com>
2019-09-05 18:25:54 -0500

    finished 25, logical NOT

--- sandbox.js
+++ sandbox.js
@@ -1,12 +1,7 @@
-// logical operators - OR || and AND &&
+// logical NOT (!)
 
-const password = 'p@ssw';
+let user = false;
 
-if(password.length >= 12 && password.includes('@')){
-    console.log('that password is mighty strong!');
-}else if(password.length >= 8 || password.includes('@') && password.length >= 5){
-    console.log('that password is strong enough!');
-} else {
-    console.log('password is not strong');
+if(!user){
+    console.log('you must be logged in to continue');
 }
-

36df19d233dc653274fa21dfef780e41d3549bf8
SummerRollison <me@summerrollison.com>
2019-09-05 17:48:38 -0500

    finished lesson 20

--- sandbox.js
+++ sandbox.js
@@ -1,14 +1,13 @@
-// for loops
-
-for(let i = 0; i < 5; i++){
-console.log(`in loop ${i}`);
-} //no ; at end of loop
-
-console.log('loop finished');
-
+// while loops
 const names = ['shaun', 'mario', 'luigi'];
-for(let i = 0; i < names.length; i++){
-  // console.log(names[i]); 
-  let html = `<div>${names[i]}</div>`; 
-  console.log(html);
+let i = 0;
+
+// while(i < 5){
+//     console.log('in loop: ', i);
+//     i++;
+// }
+
+while(i < names.length){
+    console.log(names[i]);
+    i++;
 }

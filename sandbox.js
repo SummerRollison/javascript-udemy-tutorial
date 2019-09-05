f3da71288aece3ed723c8dcad9ba97b153e90d34
SummerRollison <me@summerrollison.com>
2019-09-05 17:41:27 -0500

    finished lesson 19

--- sandbox.js
+++ sandbox.js
@@ -1,31 +1,14 @@
-// type conversion
+// for loops
 
-let score = '100';
-console.log(score + 1);
-// output: 1001 
-// need type conversion to turn string into number
+for(let i = 0; i < 5; i++){
+console.log(`in loop ${i}`);
+} //no ; at end of loop
 
-score = Number(score); // turnes score into a number
-console.log(score + 1);
-// output: 101
+console.log('loop finished');
 
-console.log(typeof score);
-// output: number
-
-let result = Number('hello');
-console.log(result);
-// output: NaN
-
-let result1 = String(50);
-console.log(typeof result1);
-
-let result2 = Boolean(100);
-console.log(result2);
-// output: true
-
-let result3 = Boolean(0);
-console.log(result3);
-// output: false
-
-let result4 = Boolean('0');
-console.log(result4); //true, strings of any length are true. no length equals false
+const names = ['shaun', 'mario', 'luigi'];
+for(let i = 0; i < names.length; i++){
+  // console.log(names[i]); 
+  let html = `<div>${names[i]}</div>`; 
+  console.log(html);
+}

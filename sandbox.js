35bcea5091de68e0bbd82bdc63024d2c9ef9e42a
SummerRollison <me@summerrollison.com>
2019-09-04 20:49:20 -0500

    completed lesson 13

--- sandbox.js
+++ sandbox.js
@@ -1,22 +1,41 @@
-// template strings
-const title = 'Best reads of 2019';
-const author = 'Summer';
-const likes = 30;
+let ninjas = ['shaun', 'ryu', 'chun-li'];
+console.log(ninjas[1]);
+// ouput: ryu
 
-// concatenation way
-let result = 'the blog called ' + title + ' by ' + author + ' has ' + likes + ' likes';
-console.log(result);
-// output: the blog called Best reads of 2019 by Summer has 30 likes
+ninjas[1] = 'ken';
+console.log(ninjas[1]);
+//output: ken
 
-// template string way or template literal
-let result = `the blog called ${title} by ${author} has ${likes} likes`;
-console.log(result);
-//  same output, easier to type and manage
+let ages = [20,25,30,35];
+console.log(ages[2]);
+// output: 30
 
-// creating html templates
-let html = `
-<h2>${title}</h2>
-<p>by ${author}</p>
-<span>this blog has ${likes} likes</span>
-`;
-console.log(html);
+let random = ['shaun', 'crystal', 30, 20];
+console.log(random);
+
+console.log(ninjas.length);
+//output: 3, because there are 3 outputs in ninjas
+
+// array methods
+
+let result0 = ninjas.join(',');
+console.log(result0);
+//output: shaun,ryu,chun-li
+
+
+let result1 = ninjas.indexOf('chun-li');
+console.log(result1);
+//output: 2
+
+let result2 = ninjas.concat(['ken', 'crystal']);
+console.log(result2);
+// output: "shaun", "ken", "chun-li", "ken", "cryston"
+
+let result3 = ninjas.push('ken');
+// console.log(result3);
+// output: 4
+// this is adding 'ken' to the end of ninjas, counting the strings
+// alters the original value, console.log(ninjas) would now output 4
+
+result3 = ninjas.pop();
+console.log(ninjas);

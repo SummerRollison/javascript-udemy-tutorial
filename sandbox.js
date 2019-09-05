3acc79afe65e030ca130f85e1e3ad07c2e09a4c1
SummerRollison <me@summerrollison.com>
2019-09-04 20:29:29 -0500

    completed lesson 12

--- sandbox.js
+++ sandbox.js
@@ -1,15 +1,22 @@
-// common string methods
+// template strings
+const title = 'Best reads of 2019';
+const author = 'Summer';
+const likes = 30;
 
-let email = 'me@summerrollison.com';
-
-// let result = email.lastIndexOf('n');
-
-// let result = email.slice(0,5);
-
-// let result = email.substr(4,10);
-
-// let result = email.replace('m', 'w');
-
-let result = email.replace('n' 'w');
-
+// concatenation way
+let result = 'the blog called ' + title + ' by ' + author + ' has ' + likes + ' likes';
 console.log(result);
+// output: the blog called Best reads of 2019 by Summer has 30 likes
+
+// template string way or template literal
+let result = `the blog called ${title} by ${author} has ${likes} likes`;
+console.log(result);
+//  same output, easier to type and manage
+
+// creating html templates
+let html = `
+<h2>${title}</h2>
+<p>by ${author}</p>
+<span>this blog has ${likes} likes</span>
+`;
+console.log(html);

766184e2bed3dffbb21b7f119cfe236354d5e015
SummerRollison <me@summerrollison.com>
2019-09-05 20:20:29 -0500

    finished 33, arrow functions

--- sandbox.js
+++ sandbox.js
@@ -1,18 +1,36 @@
-// returning values
+// Regular function
 
-// const speak = function(name = 'luigi', time = 'night'){
-//     console.log(`good ${time} ${name}`);
+// const calcArea = function(radius){
+//     return 3.14 * radius**2;
+// }
+
+// arrow function
+// const calcArea = (radius) => 3.14 * radius**2;
+// const area = calcArea(5);
+// console.log('area is:', area);
+
+// const greet = function(){
+//     return 'hello, world';
 // };
 
-const calcArea = function(radius){
-    return 3.14 * radius**2;
+// const greet = () => 'hello, world';
+// const result = greet();
+// console.log(result);
+
+// const bill = function(products, tax){
+//     let total = 0;
+//     for(let i = 0; i <products.length; i++){
+//         total += products[i] + products[i] * tax;
+//     }
+//     return total;
+// }
+
+const bill = (products, tax) => {
+    let total = 0;
+    for(let i = 0; i <products.length; i++){
+        total += products[i] + products[i] * tax;
+    }
+    return total;
 }
 
-const a = calcArea(5);
-console.log(a);
-
-const calcVol = function(area){
-
-};
-
-calcVol(area);
+console.log(bill([10,15,30], 0.2));

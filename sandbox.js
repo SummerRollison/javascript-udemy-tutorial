28e9d76c76bcea105914487ee5be732fd31a3e3f
SummerRollison <me@summerrollison.com>
2019-09-05 20:04:39 -0500

    finished 32, returning values

--- sandbox.js
+++ sandbox.js
@@ -1,8 +1,18 @@
-// arguments & parameters
+// returning values
 
-const speak = function(name = 'luigi', time = 'night'){
-    console.log(`good ${time} ${name}!`);
+// const speak = function(name = 'luigi', time = 'night'){
+//     console.log(`good ${time} ${name}`);
+// };
+
+const calcArea = function(radius){
+    return 3.14 * radius**2;
+}
+
+const a = calcArea(5);
+console.log(a);
+
+const calcVol = function(area){
+
 };
 
-speak();
-speak('shaun', 'day');
+calcVol(area);

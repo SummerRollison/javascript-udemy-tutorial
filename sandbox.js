2fde56d8f21f25876ffda2eb202c51a9e26fc37e
SummerRollison <me@summerrollison.com>
2019-09-06 07:43:27 -0500

    finished 39, object literals continued

--- sandbox.js
+++ sandbox.js
@@ -1,21 +1,24 @@
-// objerct literals
+// object literals
 
 let user = {
     name: 'crystal',
     age: 30,
     email: 'crystal@thenetninja.co.uk',
     location: 'berlin',
-    blogs: ['why mac & cheese rules', '10 things to make with marmite']
+    blogs: ['why mac & cheese rules', '10 things to make with marmite'],
+    login: function(){
+        console.log('the user logged in');
+    },
+    logout: function(){
+        console.log('the user logged out');
+    },
+    logBlogs: function(){
+        
+    }
 };
 
-console.log(user);
-console.log(user.name);
+user.login();
+user.logout();
 
-user.age = 35;
-console.log(user.age);
-
-console.log(user['name']);
-user['name'] = 'chun-li';
-console.log(user['name']);
-
-console.log(typeof user);
+const name = 'mario';
+name.toUpperCase();

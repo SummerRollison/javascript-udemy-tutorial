ddc097000a96d69d272008188d8177ea4b60a06c
SummerRollison <me@summerrollison.com>
2019-09-08 19:48:17 -0500

    adding and changing content

--- sandbox.js
+++ sandbox.js
@@ -1,13 +1,21 @@
-// get an element by ID
-const title = document.getElementById('page-title');
-console.log(title);
+// const para = document.querySelector('p');
 
-// get elements by their class name
-const errors = document.getElementsByClassName('error');
-console.log(errors);
-console.log(errors[0]);
+// console.log(para.innerText);
+// para.innerText = 'ninjas are awesome!';
 
+// const paras = document.querySelectorAll('p');
+// paras.forEach(para => {
+//     console.log(para.innerText);
+//     para.innerText += ' new text';
+// });
 
-// get elements by their tag name
-const paras = document.getElementsByTagName('p');
-console.log(paras);
+const content = document.querySelector('.content');
+
+// // console.log(content.innerHTML);
+// content.innerHTML += '<h2>This is a new h2</h2>'
+
+const people = ['mario', 'luigi', 'yoshi'];
+
+people.forEach(person => {
+    content.innerHTML += `<p>${person}</p>`;
+});

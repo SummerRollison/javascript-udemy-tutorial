f7617c9e851a4bf4541653211c44b4c30663c409
SummerRollison <me@summerrollison.com>
2019-09-08 19:55:32 -0500

    getting and setting attributes

--- sandbox.js
+++ sandbox.js
@@ -1,21 +1,10 @@
-// const para = document.querySelector('p');
+const link = document.querySelector('a');
+console.log(link.getAttribute('href'));
+link.setAttribute('href', 'https://www.thenetninja.co.uk');
+link.innerText = 'The Net Ninja Website';
 
-// console.log(para.innerText);
-// para.innerText = 'ninjas are awesome!';
+const mssg = document.querySelector('p');
 
-// const paras = document.querySelectorAll('p');
-// paras.forEach(para => {
-//     console.log(para.innerText);
-//     para.innerText += ' new text';
-// });
-
-const content = document.querySelector('.content');
-
-// // console.log(content.innerHTML);
-// content.innerHTML += '<h2>This is a new h2</h2>'
-
-const people = ['mario', 'luigi', 'yoshi'];
-
-people.forEach(person => {
-    content.innerHTML += `<p>${person}</p>`;
-});
+console.log(mssg.getAttribute('class'));
+mssg.setAttribute('class', 'success');
+mssg.setAttribute('style', 'color: green;');
